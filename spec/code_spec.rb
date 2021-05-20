7cd7c1a11636f1320a60a05a1997d34ee8c756e6
Michael Abbott <michael@mjabbottdesign.com>
2021-05-19 18:35:54 -0700

    Refactor Code; refactor and update test

--- spec/code_spec.rb
+++ spec/code_spec.rb
@@ -9,18 +9,11 @@ RSpec.describe Code do
 
     it 'holds a code' do
         secret_code = Code.new
-        secret_code.generate
 
+        expect(secret_code.pattern).to eq([])
+
+        secret_code.generate
         expect(secret_code.pattern.length).to eq(4)
         expect(secret_code.pattern).to include('R', 'G', 'B', 'Y')
-    end
-
-    it 'generates random color combo' do
-        secret_code = Code.new
-        code1 = secret_code.generate
-        code2 = secret_code.generate
-        code3 = secret_code.generate
-
-        expect(code1).to_not eq(code2 || code3)
     end
 end
