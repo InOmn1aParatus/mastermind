dd72b78c9acdb697d9524bb48ab78dfe2f2e9507
Matt Kragen <mattdkragen@gmail.com>
2021-05-21 13:36:10 -0700

    Refactor print methods; create mastermind.rb

--- lib/game.rb
+++ lib/game.rb
@@ -8,18 +8,19 @@ class Game
   end
 
   def welcome
-    p 'Welcome to MASTERMIND'
-    p 'Would you like to (p)lay, read the (i)nstructions, or (q)uit?'
-    begin_game(user_input)
+    puts 'Welcome to MASTERMIND'
+    puts 'Would you like to (p)lay, read the (i)nstructions, or (q)uit?'
+    # begin_game(user_input)
   end
 
   def instructions
-    p 'I will come up with a secret 4-color code.' 
-    p 'Your job is to guess the correct colors in the correct order within 10 turns.'
-    p 'After each guess I will give you two hints to tell how close you were.'
-    p 'Guess the code... and you win!'
-    p 'Use up your turns... GAME OVER :('
-    begin_game(user_input)
+    puts 'I will come up with a secret 4-color code.' 
+    puts 'Your job is to guess the correct colors in the correct order within 10 turns.'
+    puts 'After each guess I will give you two hints to tell how close you were.'
+    puts 'Guess the code... and you win!'
+    puts 'Use up your turns... GAME OVER :('
+    puts '(p)lay? (q)uit?'
+    # begin_game(user_input)
   end
 
   # def begin_game(user_input)
